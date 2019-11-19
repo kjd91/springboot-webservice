32696d99fc8aaf89292acf07329dd678f04c6b98
Kim Jae Doo <sprtms0101@naver.com>
2019-11-20 00:17:28 +0900

    2018-11-20 프로젝트

---
+++ src/main/java/com/jojoldu/webservice/web/WebRestController.java
@@ -0,0 +1,43 @@
+package com.jojoldu.webservice.web;
+
+import com.jojoldu.webservice.dto.posts.PostsSaveRequestDto;
+import com.jojoldu.webservice.service.PostsService;
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+/**
+ * Created by jojoldu@gmail.com on 2017. 12. 24.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private PostsService postsService;
+    private Environment env;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
+}
